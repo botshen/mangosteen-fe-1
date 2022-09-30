ae07bd2876970f2def0ee5d6252bec4b089b436e
shenxin <363088847@qq.com>
2022-09-30 14:30:30 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen-fe-1/dist/',
-  plugins: [vue()]
-})
+  plugins: [vue()],
+});
