3e9fcbbf19cc8439f88788d4db29f28cc0cb3f19
shenxin <363088847@qq.com>
2022-09-30 16:07:18 +0800

    重构 main.ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import { RouterView } from "vue-router";
 
 export const App = defineComponent({
   setup() {
-    return () => <>
-      <header>导航
-        <ul>
-          <li>
-            <router-link to="/">Foo</router-link>
-          </li>
-          <li>
-            <router-link to="/about">Bar</router-link>
-          </li>
-        </ul>
-      </header>
-      <div>
-        <RouterView />
-      </div>
-      <footer>页脚</footer>
-    </>
-
-  }
-})
+    return () => (
+      <>
+        <header>
+          导航
+          <ul>
+            <li>
+              <router-link to="/">Foo</router-link>
+            </li>
+            <li>
+              <router-link to="/about">Bar</router-link>
+            </li>
+          </ul>
+        </header>
+        <div>
+          <RouterView />
+        </div>
+        <footer>页脚</footer>
+      </>
+    );
+  },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { createApp } from "vue";
 import { App } from "./App";
-import { Bar } from "./views/Bar";
-import { Foo } from "./views/Foo";
-import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [
-  { path: "/", component: Foo },
-  { path: "/about", component: Bar },
-];
+import { createRouter } from "vue-router";
+import { routes } from "./config/routes";
+import { history } from "./shared/history";
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+const router = createRouter({ history, routes });
 
 const app = createApp(App);
 app.use(router);
