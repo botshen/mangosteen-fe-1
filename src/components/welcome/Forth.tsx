5bc0efde0821e6468a57326c54525cb44a93f8a0
shenxin <363088847@qq.com>
2022-11-16 14:28:40 +0800

    究极重构

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,23 +1,20 @@
-import { defineComponent } from 'vue'
 import { RouterLink } from 'vue-router'
 import cloud from '../../assets/icons/cloud.svg'
 import s from './WelcomeLayout.module.scss'
 import { WelcomeLayout } from './WelcomeLayout'
-export const Forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
-        {{
-          icon: () => <img class={s.icon} src={cloud} />,
-          title: () => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
-          buttons: () => <>
-            <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-            <RouterLink to="/start" >完成</RouterLink>
-            <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          </>,
+export const Forth = () => (
+  <WelcomeLayout>
+    {{
+      icon: () => <img class={s.icon} src={cloud} />,
+      title: () => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+        <RouterLink to="/start" >完成</RouterLink>
+        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+      </>,
 
-        }}
-      </WelcomeLayout>
-    )
-  },
-})
+    }}
+  </WelcomeLayout>
+)
+
+Forth.displayName = 'Forth'
