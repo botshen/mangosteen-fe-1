bbf25f6743914737c466560e99d8dbc8242471a6
botshen <363088847@qq.com>
2022-11-20 23:39:36 +0800

    添加 tag 相关页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,17 +1,20 @@
-import type { RouteRecordRaw } from 'vue-router'
-import { ItemCreate } from '../components/item/ItemCreate'
-import { ItemList } from '../components/item/ItemList'
-import { First } from '../components/welcome/First'
-import { FirstActions } from '../components/welcome/FirstActions'
-import { Forth } from '../components/welcome/Forth'
-import { ForthActions } from '../components/welcome/ForthActions'
-import { Second } from '../components/welcome/Second'
-import { SecondActions } from '../components/welcome/SecondActions'
-import { Third } from '../components/welcome/Third'
-import { ThirdActions } from '../components/welcome/ThirdActions'
-import { ItemPage } from '../views/ItemPage'
-import { StartPage } from '../views/StartPage'
-import { Welcome } from '../views/Welcome'
+import { RouteRecordRaw } from "vue-router";
+import { ItemCreate } from "../components/item/ItemCreate";
+import { ItemList } from "../components/item/ItemList";
+import { TagCreate } from "../components/tag/TagCreate";
+import { TagEdit } from "../components/tag/TagEdit";
+import { First } from "../components/welcome/First";
+import { FirstActions } from "../components/welcome/FirstActions";
+import { Forth } from "../components/welcome/Forth";
+import { ForthActions } from "../components/welcome/ForthActions";
+import { Second } from "../components/welcome/Second";
+import { SecondActions } from "../components/welcome/SecondActions";
+import { Third } from "../components/welcome/Third";
+import { ThirdActions } from "../components/welcome/ThirdActions";
+import { ItemPage } from "../views/ItemPage";
+import { StartPage } from "../views/StartPage";
+import { TagPage } from "../views/TagPage";
+import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/welcome' },
@@ -20,19 +23,25 @@ export const routes: RouteRecordRaw[] = [
     component: Welcome,
     children: [
       { path: '', redirect: '/welcome/1' },
-      { path: '1', name: 'Welcome1', components: { main: First, footer: FirstActions } },
-      { path: '2', name: 'Welcome2', components: { main: Second, footer: SecondActions } },
-      { path: '3', name: 'Welcome3', components: { main: Third, footer: ThirdActions } },
-      { path: '4', name: 'Welcome4', components: { main: Forth, footer: ForthActions } },
-    ],
+      { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
+      { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
+      { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
+      { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
+    ]
   },
   { path: '/start', component: StartPage },
   {
-    path: '/items',
-    component: ItemPage,
+    path: '/items', component: ItemPage,
     children: [
       { path: '', component: ItemList },
       { path: 'create', component: ItemCreate },
-    ],
+    ]
   },
+  {
+    path: '/tags', component: TagPage,
+    children: [
+      {path: 'create', component: TagCreate},
+      {path: ':id', component: TagEdit}
+    ]
+  }
 ]
