38c8ad787c735fa5af707260e2a6805efd8f1d32
botshen <363088847@qq.com>
2022-11-24 09:42:12 +0800

    扩展了 Tabs 组件，支持 classPrefix

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,15 +1,39 @@
-import type { PropType } from 'vue'
-import { defineComponent } from 'vue'
-import s from './ItemList.module.scss'
+import { defineComponent, PropType, ref } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
+import s from './ItemList.module.scss';
 export const ItemList = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>,
-    },
+      type: String as PropType<string>
+    }
   },
   setup: (props, context) => {
+    const refSelected = ref('本月')
     return () => (
-      <div class={s.wrapper}>hi</div>
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu" />,
+          default: () => (
+            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+              <Tab name="本月">
+                list 1
+              </Tab>
+              <Tab name="上月">
+                list 2
+              </Tab>
+              <Tab name="今年">
+                list 3
+              </Tab>
+              <Tab name="自定义时间">
+                list 4
+              </Tab>
+            </Tabs>
+          )
+        }
+      }</MainLayout>
     )
-  },
+  }
 })
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,8 +1,10 @@
-import type { PropType } from 'vue'
-import { defineComponent } from 'vue'
-import s from './Tabs.module.scss'
+import { defineComponent, PropType } from 'vue';
+import s from './Tabs.module.scss';
 export const Tabs = defineComponent({
   props: {
+    classPrefix: {
+      type: String
+    },
     selected: {
       type: String as PropType<string>,
       required: false,
@@ -10,21 +12,25 @@ export const Tabs = defineComponent({
     onUpdateSelected: {
       type: Function as PropType<(name: string) => void>,
       required: false,
-    },
+    }
   },
   setup: (props, context) => {
     return () => {
       const tabs = context.slots.default?.()
-      if (!tabs)
-        return () => null
+      if (!tabs) return () => null
       for (let i = 0; i < tabs.length; i++) {
-        if (tabs[i].type !== Tab)
+        if (tabs[i].type !== Tab) {
           throw new Error('<Tabs> only accepts <Tab> as children')
+        }
       }
-      return <div class={s.tabs}>
-        <ol class={s.tabs_nav}>
+      const cp = props.classPrefix
+      return <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
           {tabs.map(item =>
-            <li class={item.props?.name === props.selected ? s.selected : ''}
+            <li class={[
+              item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '',
+              cp + '_tabs_nav_item'
+            ]}
               onClick={() => context.emit('update:selected', item.props?.name)}
             >
               {item.props?.name}
@@ -35,18 +41,18 @@ export const Tabs = defineComponent({
         </div>
       </div>
     }
-  },
+  }
 })
 
 export const Tab = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>,
-    },
+      type: String as PropType<string>
+    }
   },
   setup: (props, context) => {
     return () => (
       <div>{context.slots.default?.()}</div>
     )
-  },
+  }
 })
